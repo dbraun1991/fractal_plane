a4429e7ee04953a512a66ce5c8f350a2866ccc31
Dominik Braun <dominik.braun@schauinsland-reisen.de>
2025-01-04 13:48:53 +0100

    Add frontend framework - add tailwind  - beta v6

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as d3 from 'd3';
+import './styles/styles.css';
 
 import { drawSquares, drawCubicBezierSquaresSymmetric } from './square';
 import { drawCubicBezierTrianglesSymmetric } from './triangle';
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html', 
+    './src/**/*.{js,ts,jsx,tsx,html}' // adjust paths based on your file structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
